343a382f9d31eb99d9981c99bb380a1401162717
Ana Bollain <ana.bollain@gmail.com>
2022-11-09 21:06:10 +0100

    Update HTML & add CSS styles

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@ const gameResult = document.querySelector('.js-result');
 const replayBtn = document.querySelector('.js-replay-btn');
 const resulBtn = document.querySelector('.js-result-btn');
 
+const cardEl = document.querySelector('.card-template');
+
 //Start with replay button hidden
 replayBtn.classList.add('hidden');
 
@@ -15,35 +17,62 @@ replayBtn.classList.add('hidden');
 const userResultEl = document.querySelector('.js-result-user');
 const compResultEl = document.querySelector('.js-result-computer');
 
-//Create objects for evil races (computer side)
+//Create objects for good races (user side)
 
-const race1 = {
-    name: 'Sureños malos con fuerza',
+const goodRace1 = {
+    name: 'Sir Cup',
+    value: 1
+}
+
+const goodRace2 = {
+    name: 'Big Nose',
     value: 2
 }
 
-const race2 = {
-    name: 'Orcos con fuerza',
-    value: 2
-}
-
-const race3 = {
-    name: 'Goblins con fuerza',
-    value: 2
-}
-
-const race4 = {
-    name: 'Huargos con fuerza',
+const goodRace3 = {
+    name: 'Ballon Head',
     value: 3
 }
 
-const race5 = {
-    name: 'Trolls con fuerza',
+const goodRace4 = {
+    name: 'Weird Phantom',
+    value: 4
+}
+
+const goodRace5 = {
+    name: 'Crazy Monkey',
+    value: 5
+}
+
+//Create objects for evil races (computer side)
+
+const evilRace1 = {
+    name: 'Sad Alien',
+    value: 2
+}
+
+const evilRace2 = {
+    name: 'Fluffy',
+    value: 2
+}
+
+const evilRace3 = {
+    name: 'Angry Rhino',
+    value: 2
+}
+
+const evilRace4 = {
+    name: 'Mad',
+    value: 3
+}
+
+const evilRace5 = {
+    name: 'Creepy Smile',
     value: 5
 }
 
 //Create array for evil races (computer side)
-const evilRaces = [race1, race2, race3, race4, race5];
+const evilRaces = [evilRace1, evilRace2, evilRace3, evilRace4, evilRace5];
 
 
 //Function to calculate random number
@@ -55,6 +84,8 @@ function getRandomNumber(max) {
 
 let userResult = 0;
 let computerResult = 0;
+let totalMoves = 0;
+
 //Function to compare values from each side (user and computer) when clicking button
 function compareResult(){
     const randomValue = getRandomNumber(4);
@@ -63,12 +94,12 @@ function compareResult(){
     console.log(raceValue);
     console.log(evilRaces[randomValue]);
     if (raceValue < randomRaceValue){
-        gameResult.innerHTML = 'Ha ganado el Ejército del Mal! Vuelve a Intentarlo.';
+        resulBtn.innerHTML = 'Ha ganado el Ejército del Mal! Vuelve a Intentarlo.';
         computerResult += 1;
     }else if (raceValue === randomRaceValue){
-        gameResult.innerHTML = 'Empate';
+        resulBtn.innerHTML = 'Empate';
     }else{
-        gameResult.innerHTML = 'Ha ganado el Ejército del Bien! Enhorabuena.';
+        resulBtn.innerHTML = 'Ha ganado el Ejército del Bien! Enhorabuena.';
         userResult += 1;
     }
 }
@@ -77,15 +108,14 @@ function compareResult(){
 function paintResult(){
     userResultEl.innerHTML = `Jugador: ${userResult}`;
     compResultEl.innerHTML = `Computadora: ${computerResult}`;
-    if(userResult === 10 || computerResult === 10){
+}
+
+function updateCounter(){
+    totalMoves++;
+    gameResult.innerHTML = totalMoves;
+    if(totalMoves === 10){
         replayBtn.classList.remove('hidden');
         playBtn.classList.add('hidden');
-    }
-    if(userResult === 10){
-        resulBtn.innerHTML = `Has ganado el juego`;
-    }
-    if(computerResult === 10){
-        resulBtn.innerHTML = `Has perdido el juego`;
     }
 }
 
@@ -94,6 +124,7 @@ function handleClick (event){
     event.preventDefault();
     compareResult();
     paintResult();
+    updateCounter();
 }
 
 //Event listener play button
@@ -104,6 +135,8 @@ replayBtn.addEventListener('click', (event)=>{
     event.preventDefault();
     userResult = 0;
     computerResult = 0;
+    totalMoves = 0;
+    gameResult.innerHTML = totalMoves;
     userResultEl.innerHTML = userResult;
     compResultEl.innerHTML = computerResult;
     resulBtn.innerHTML = `¡Comienza la batalla!`;
